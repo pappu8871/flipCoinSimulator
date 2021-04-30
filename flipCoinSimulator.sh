3855273bf69032a0256b1db6df789fa776438d5f
your username <59005110+pappu8871@users.noreply.github.com>
2021-04-30 20:55:30 +0530

    Added:show numbers of times head and tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,39 @@
+
 #!/bin/bash
+RANDOM=$$
+
+PIPS=2
+MAXTHROWS=10
+throw=0
+
 Head=0
-Tils=1
-REsult=$((RANDOM%2))
-if [[ ${Result} -eq 0 ]];
-then
-    echo "HEADS"
-     elif [[${Result} -eq 1 ]];
-     echo "Tails"
-fi
+Tails=0
+print_result ()
+{
+echo
+echo "Head =   $Head"
+echo "Tails =   $Tails"
+
+echo
+}
+
+update_count()
+{
+case "$1" in
+  0) let "Head += 1";;
+  1) let "Tails += 1";;
+
+esac
+}
+
+echo
+
+while [ "$throw" -lt "$MAXTHROWS" ]
+do
+  let "die1 = RANDOM % $PIPS"
+  update_count $die1
+  let "throw += 1"
+done
+
+print_result
+exit 0
